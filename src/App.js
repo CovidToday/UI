cb2a6aabe43b48b0f4e8cd02276472863e3de80d
Aditya <adigarg97@gmail.com>
2020-06-06 02:33:15 +0530

    homepage bottom styling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import { Line, Chart } from 'react-chartjs-2';
-import { Container, Row, Col, Dropdown, Card, Button, Popover, OverlayTrigger } from 'react-bootstrap';
+import { Container, Row, Col, Dropdown, Card, Button, Popover, OverlayTrigger, CardGroup } from 'react-bootstrap';
 import Header from "./images/header.png"
 import Footer from "./images/footer.jpg"
 import informationIcon from "./images/information_icon.png";
@@ -1416,77 +1416,80 @@ class App extends Component {
 					</div>
 
 					<div className="home-text" ref={this.textDivRef}>
-						<Card>
-							<Card.Body>
-								<Card.Title className="top-text-title" style={{ fontWeight: "bold", fontStyle: "italic" }}>{`How fast is the spread? (Transmission indicators)`}</Card.Title>
-							</Card.Body>
+					<div className="for-the-people-heading" style={{padding: "10px"}}>How fast is the spread? (Transmission indicators)</div>
+					<CardGroup>
+						<Card style={{background: "#e8e8e8"}}>
 							<Card.Body>
 								<Card.Title className="top-text-title" style={{ fontWeight: "bold" }}>{`Effective Reproduction Number (Rt)`}</Card.Title>
 								<Card.Text className="top-text-body">
-									<div><span style={{ fontStyle: "italic" }}>Rt is the average number of people infected by a single case, at a particular time t during the outbreak.</span> It shows us
-										the rate of spread of the virus. When Rt reaches below 1 (epidemic threshold), we can say that the outbreak has been brought
-										under control. Serial monitoring of Rt for each state tells us the severity of the outbreak in an area, and guides administrators
-										to fine-tune the level of control measures required to bring the Rt under 1. As changes in transmission correlate with
-										control measures, we can assess the efficacy of different interventions by comparing the change in Rt after their implementation. <br />
-										Green: Below 1 <br />
-										Red: Above 1</div>
+									<div><span style={{ fontStyle: "italic" }}>Rt is the average number of people infected by a single case, at a particular time 
+									t during the outbreak.</span>  WHO recommends this metric as the key measure to know the rate of spread of the virus. When Rt 
+									reaches below 1, we can say that the outbreak has been brought under control. Tracking the regional Rt tells us the severity of 
+									the outbreak in each state, and guides administrators to fine-tune the level of control measures required to bring the Rt under 
+									1. As changes in transmission correlate with control measures, we can assess the efficacy of different measures by comparing the 
+									change in Rt after their implementation. </div>
 								</Card.Text>
 							</Card.Body>
+						</Card>	
+						<span style={{width: "2%"}}> </span>
+						<Card style={{background: "#e8e8e8"}}>	
 							<Card.Body>
 								<Card.Title className="top-text-title" style={{ fontWeight: "bold" }}>{`Mobility Index`}</Card.Title>
 								<Card.Text className="top-text-body">
-									<div><span style={{ fontStyle: "italic" }}>This indicates the change in frequency and length of visits at different places compared to a baseline level from
-										Jan 3 to Feb 6, 2020.</span> It shows us the effect of lockdown and behavioural change on the movement of people, reflecting the
-										strictness and enforcement of social distancing in different states. We have introduced this parameter experimentally
-										considering that mobility has a direct effect on disease spread, however there is no evidence yet that this specific mobility
-										index is correlated with local transmission. <br /> Data Source: Google COVID19 Community Mobility Reports</div>
-								</Card.Text>
-							</Card.Body>
-							<Card.Body></Card.Body>
-							<Card.Body>
-								<Card.Title className="top-text-title" style={{ fontWeight: "bold", fontStyle: "italic" }}>{`Are we testing enough? (Testing indicators)`}</Card.Title>
-							</Card.Body>
-							<Card.Body>
-								<Card.Title className="top-text-title" style={{ fontWeight: "bold" }}>{`Test Positivity Rate`}</Card.Title>
-								<Card.Text className="top-text-body">
-									<div><span style={{ fontStyle: "italic" }}>It is the percent of COVID-19 tests done that come back positive.</span> A low positivity rate may mean that testing levels
-										are sufficient for the scale of the epidemic and surveillance is penetrating the community enough to detect any resurgence.
-										In contrast, a high positivity rate indicates that testing is relatively limited to people with high suspicion of COVID-19 and
-										may miss new chains of transmission in the community. Test Positivity Rate is a better indicator of testing adequacy than Tests
-										Per Million, as testing coverage should be seen relative to the size of the epidemic rather than the size of the population.
-										(https://coronavirus.jhu.edu/testing/international-comparison) The WHO has recommended that the daily positivity rate be below
-										5% for atleast two weeks before relaxing public health measures. We report daily positivity rate (as 7-day moving averages) and
-										cumulative positivity rate (which includes all tests done till date). <br />
-										Red: More than 10% <br /> Yellow: Between 5% and 10% <br /> Green: Less than 5% (based on WHO criteria)</div>
-								</Card.Text>
-							</Card.Body>
-							<Card.Body>
-								<Card.Title className="top-text-title" style={{ fontWeight: "bold" }}>{`Corrected Case Fatality Rate (CFR)`}</Card.Title>
-								<Card.Text className="top-text-body">
-									<div>The Crude CFR is equal to the deaths till date divided by the cases till date. This naive estimate of CFR is known to be
-									biased in ongoing outbreaks, primarily due to two factors- the delay between time of case confirmation and time of death, and
-									the under-reporting of cases due to limitations in testing coverage. The Corrected CFR presented here corrects for the first bias,
-									by adjusting the denominator to reflect the number of cases where death would have been reported if it had occurred, based on
-									known estimates of delay from confirmation to death. The variation in Corrected CFR across states would then reflect the degree
-										of under-reporting or testing adequacy in a particular state. <br />
-										Red: More than 10% <br /> Yellow: Between 5% and 10% <br /> Green: Less than 5% </div>
-								</Card.Text>
-							</Card.Body>
-							<Card.Body>
-								<Card.Title className="top-text-title" style={{ fontWeight: "bold" }}>{`Tests Per Million`}</Card.Title>
-								<Card.Text className="top-text-body">
-									<div style={{ fontStyle: "italic" }}>It is the total number of tests done per 10,00,000 people.</div>
-								</Card.Text>
-							</Card.Body>
-							<Card.Body>
-								<Card.Title className="top-text-title" style={{ fontWeight: "bold" }}>{`For The People, By The People`}</Card.Title>
-								<Card.Text className="top-text-body">
-									<div>COVID TODAY is an initiative by iCART, a multidisciplinary volunteer team of passionate doctors, researchers, coders,
-										and public health experts from institutes across India. <span style={{ fontWeight: "bold", fontStyle: "italic" }}>Anyone can contribute to this project</span> -
-										<a className="link-text" onClick={() => this.setState({ selectedView: "Contribute" })}>click here if you want to pitch in.</a></div>
+									<div><span style={{ fontStyle: "italic" }}>This indicates the change in the amount of movement of people at various places 
+									compared to that before lockdown</span>  It shows us the effect of lockdown and behavioural change on the movement of people, 
+									and how this changes as restrictions are relaxed in a graded manner. We have introduced this parameter experimentally considering 
+									that mobility has a direct effect on disease spread, however there is no evidence yet that the mobility indices shown directly 
+									correlate with local transmission.</div>
 								</Card.Text>
 							</Card.Body>
 						</Card>
+					</CardGroup>
+					<div className="for-the-people-heading" style={{padding: "10px"}}>Are we testing enough? (Testing indicators)</div>
+					<CardGroup>
+						<Card style={{background: "antiquewhite"}}>
+							<Card.Body>
+								<Card.Title className="top-text-title" style={{ fontWeight: "bold" }}>{`Test Positivity Rate`}</Card.Title>
+								<Card.Text className="top-text-body">
+									<div><span style={{ fontStyle: "italic" }}>It is the percent of COVID-19 tests done that come back positive.</span> A low positivity 
+									rate means that testing levels are sufficient for the scale of the epidemic and surveillance is penetrating the community enough to 
+									detect any resurgence. In contrast, a high positivity rate indicates that testing is relatively limited to people with high suspicion 
+									of COVID-19 and may miss new chains of transmission in the community. The WHO recommends that the daily positivity rate be below 5% 
+									for atleast two weeks before relaxing public health measures. Test Positivity Rate is a better indicator of testing adequacy than 
+									Tests Per Million, as testing coverage should be seen </div>
+								</Card.Text>
+							</Card.Body>
+						</Card>
+						<span style={{width: "2%"}}> </span>
+						<Card style={{background: "antiquewhite"}}>	
+							<Card.Body>
+								<Card.Title className="top-text-title" style={{ fontWeight: "bold" }}>{`Corrected Case Fatality Rate`}</Card.Title>
+								<Card.Text className="top-text-body">
+									<div>The Crude CFR is equal to the deaths till date divided by the cases till date. This naive estimate of CFR is known to be biased in 
+									ongoing outbreaks, primarily due to two factors- the delay between time of case confirmation and time of death, and the under-reporting 
+									of cases due to limitations in testing coverage. The Corrected CFR presented here corrects for the first bias, by adjusting the 
+									denominator to reflect the number of cases where death would have been reported if it had occurred, based on known estimates of 
+									delay from confirmation to death. <span style={{ fontStyle: "italic" }}>The variation in Corrected</span></div>
+								</Card.Text>
+							</Card.Body>
+						</Card>
+					</CardGroup>	
+					</div>
+					<div className="disclaimer">The raw data sources and detailed method of calculation is provided in the 
+						<a className="link-text" onClick={() => this.setState({ selectedView: "Methods" })}> Methods</a>. page. 
+						Caution should be used in interpretation as the transmission and testing indicators are not entirely independent, and one may affect the other. 
+						We use best practices in all calculations, however some inadvertent errors may creep in despite our efforts. 
+						<a className="link-text" onClick={() => this.setState({ selectedView: "Contribute" })}> Report an error.</a></div>
+					<div className="divider"> </div>
+					<div className="for-the-people">
+						<div className="for-the-people-heading">For The People, By The People</div>
+						<div className="for-the-people-text">COVID TODAY is an initiative by iCART, a multidisciplinary volunteer team of passionate doctors, 
+						researchers, coders, and public health experts from institutes across India. 
+						<a className="link-text" onClick={() => this.setState({ selectedView: "Team" })}> Learn more about the team</a>. This pandemic demands everyone to 
+						come together so that we can gradually move towards a new normal in the coming months while ensuring those who are vulnerable are protected. 
+						We envisage this platform to grow with your contribution and we welcome anyone who can contribute meaningfully to the project. Head over to 
+						the <a className="link-text" onClick={() => this.setState({ selectedView: "Contribute" })}>Contribute page</a> to see how you can pitch in.
+						</div>
 					</div>
 				</>}
 				{selectedView === "Methods" && <div className="App"><Methods /></div>}
